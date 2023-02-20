01b4876035a77b90d5a9328abc2139ab35e36d56
Cherednyk Yaroslav <azazely85@gmail.com>
2023-02-20 21:06:53 +0200

    update tables

---
+++ database/migrations/2023_02_20_172152_update_user_word_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_word', function (Blueprint $table) {
+            $table->timestamps();
+            $table->dateTime('start_repeat')->nullable();
+            $table->bigIncrements('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

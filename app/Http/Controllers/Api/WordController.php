6248c657f0c645000e80803eecb07936687a6f72
Shifty <azazely85@gmail.com>
2025-08-29 16:16:25 +0300

    Remove commented-out translation logic in getWord method of WordController to enhance code clarity and maintainability.

--- app/Http/Controllers/Api/WordController.php
+++ app/Http/Controllers/Api/WordController.php
@@ -347,35 +347,6 @@ class WordController extends Controller
         $data = $dom->saveXML($clearData);
         $translate = '';
 
-        //        if (!$translate) {
-        //            $baseService = new BaseService('https://e2u.org.ua/s?w=', [], false, 'dict');
-        //            $result = $baseService->getContents($wordName . '&dicts=all');
-        //            libxml_use_internal_errors(true);
-        //            $domUa = new \DOMDocument();
-        //            $domUa->loadHTML($result);
-        //            $clearDataUa = $domUa->getElementById('table_17');
-        //            if (!$clearDataUa) {
-        //                $clearDataUa = $domUa->getElementById('table_2');
-        //            }
-        //            if (!$clearDataUa) {
-        //                $clearDataUa = $domUa->getElementById('table_1');
-        //            }
-        //            if (!$clearDataUa) {
-        //                $clearDataUa = $domUa->getElementById('table_18');
-        //            }
-        //
-        //            if ($clearDataUa) {
-        //                $dataUa = $this->getElementsByClass($clearDataUa, 'td', 'result_row_main');
-        //                foreach ($dataUa as $element) {
-        //                    $translate .= $element->nodeValue . ', ';
-        //                }
-        //                $translate = stristr($translate, '1');
-        //                $pos = strpos($translate, '2');
-        //                $translate = substr($translate, 0, $pos);
-        //                $translate = mb_substr($translate, 2, mb_strlen($translate));
-        //            }
-        //        }
-
         if (!$translate) {
             $baseService = new BaseService(
                 'https://dict.com/%D0%B0%D0%BD%D0%B3%D0%BB%D1%96%D0%B8%D1%81%D1%8C%D0%BA%D0%BE-%D1%83%D0%BA%D1%80%D0%B0%D1%96%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B8/',
